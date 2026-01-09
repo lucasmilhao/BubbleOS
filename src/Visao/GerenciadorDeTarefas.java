c82e83d3c3746b403ac1255aa28f68eb46c6977e
Lucas Milhão <lucasmilhao683@gmail.com>
2026-01-09 12:53:58 -0300

    Add image rendering to GerenciadorDeTarefas

--- src/Visao/GerenciadorDeTarefas.java
+++ src/Visao/GerenciadorDeTarefas.java
@@ -3,16 +3,20 @@ package Visao;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class GerenciadorDeTarefas extends JPanel {
 
     private Dimension d;
+    private Image imagem;
     private int ALTURA;
     private int LARGURA;
 
     public GerenciadorDeTarefas(Dimension d) {
         this.ALTURA = 65;
+        this.imagem = new ImageIcon(getClass().getResource("/Imagens/tarefas.png")).getImage();
         this.LARGURA = (int) d.getWidth();
         this.d = new Dimension(LARGURA, ALTURA);
         this.setPreferredSize(this.d);
@@ -27,7 +31,7 @@ public class GerenciadorDeTarefas extends JPanel {
 
     public void draw(Graphics g) {
         g.setColor(new Color(19, 89, 158));
-        g.fillRect(0, (int) d.getHeight() - 65, (int) d.getWidth(), 65);
+        g.drawImage(imagem, 0, (int) d.getHeight() - 65, (int) d.getWidth(), 65, null);
     }
 
 }
